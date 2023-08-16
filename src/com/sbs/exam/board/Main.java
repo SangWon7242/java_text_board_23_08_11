e735a43240a2bd498a7c9dbf55f36ce63fee62da
SangWon7242 <gotkdqja@gmail.com>
2023-08-16 15:30:40 +0900

    C:/Program Files/Git/usr/article/list로 입력하면 게시물 리스트 출력 시작

--- src/com/sbs/exam/board/Main.java
+++ src/com/sbs/exam/board/Main.java
@@ -1,5 +1,7 @@
 package com.sbs.exam.board;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
@@ -8,6 +10,13 @@ public class Main {
 
     int articlesLastId = 0;
     Article lastArticle = null;
+    List<Article> articles = new ArrayList<>();
+    
+    // 테스트 데이터 3개 등록 시작
+    articles.add(new Article(1, "제목1", "내용1"));
+    articles.add(new Article(2, "제목2", "내용2"));
+    articles.add(new Article(3, "제목3", "내용3"));
+    // 테스트 데이터 3개 등록 끝
 
     System.out.println("== 게시판 v 0.1 ==");
     System.out.println("== 프로그램 시작 ==");
@@ -48,8 +57,24 @@ public class Main {
         System.out.printf("제목 : %s\n", article.title);
         System.out.printf("내용 : %s\n", article.content);
       }
+      else if(cmd.equals("/usr/article/list")) {
+        System.out.println("-- 게시물 리스트 --");
+        System.out.println("-------------------");
+        System.out.println("번호 / 제목");
+        System.out.println("-------------------");
+
+        /*
+        for(int i = 0; i < articles.size(); i++) {
+          Article article = articles.get(i);
+          System.out.printf("%d / %s\n", article.id, article.title);
+        }
+        */
+        for(Article article : articles) {
+          System.out.printf("%d / %s\n", article.id, article.title);
+        }
+      }
       else {
-        System.out.printf("입력된 명령어 : %s\n", cmd);
+        System.out.println("잘못 된 명령어 입니다.");
       }
     }
 
