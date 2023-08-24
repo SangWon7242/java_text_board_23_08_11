2c88df726a13932f8bb860b2101bdbc56ebe5bb3
SangWon7242 <gotkdqja@gmail.com>
2023-08-24 15:38:43 +0900

    클래스별 파일 분리

--- src/com/sbs/exam/board/Main.java
+++ src/com/sbs/exam/board/Main.java
@@ -1,6 +1,9 @@
 package com.sbs.exam.board;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Scanner;
 
 public class Main {
   static int articlesLastId = 0;
@@ -241,80 +244,7 @@ public class Main {
   }
 }
 
-class Article {
-  int id;
-  String title;
-  String content;
-
-  Article(int id, String title, String content) {
-    this.id = id;
-    this.title = title;
-    this.content = content;
-  }
-
-  @Override
-  public String toString() {
-    return String.format("{id : %d, title : \"%s\", content : \"%s\"}", id, title, content);
-  }
-}
-
-class Rq {
-  private String url;
-  private Map<String, String> params;
-  private String urlPath;
-
-  Rq(String url) {
-    this.url = url;
-    urlPath = Util.getUrlPathFromUrl(this.url);
-    params = Util.getParamsFromUrl(this.url);
-  }
-
-  public Map<String, String> getParams() {
-    return params;
-  }
-
-  public String getUrlPath() {
-    return urlPath;
-  }
-
-}
-
-class Util {
-  static Map<String, String> getParamsFromUrl(String url) {
-    Map<String, String> params = new HashMap<>();
-
-    String[] urlBits = url.split("\\?", 2);
-
-    if (urlBits.length == 1) {
-      return params;
-    }
-
-    for (String bit : urlBits[1].split("&")) {
-      String[] bitBits = bit.split("=", 2);
-
-      if (bitBits.length == 1) {
-        continue;
-      }
-
-      params.put(bitBits[0], bitBits[1]);
-    }
-
-    return params;
-  }
-
-  static String getUrlPathFromUrl(String url) {
-    return url.split("\\?", 2)[0];
-  }
 
 
-  // 이 함수는 원본리스트를 훼손하지 않고, 새 리스트를 만든다.
-  // 즉 정렬이 반대인 복사본 리스트를 반환한다.
-  public static <T> List<T> reverseList(List<T> list) {
-    List<T> reverse = new ArrayList<>(list.size());
 
-    for (int i = list.size() - 1; i >= 0; i--) {
-      reverse.add(list.get(i));
-    }
-    return reverse;
-  }
-}
+
